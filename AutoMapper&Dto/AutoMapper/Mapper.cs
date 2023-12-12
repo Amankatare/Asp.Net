f48f5a7427315e764dd9f59f696368a73e309009
Aman katare <78726619+Amankatare@users.noreply.github.com>
2023-12-12 22:15:00 +0530

    Add files via upload

---
+++ AutoMapper&Dto/AutoMapper/Mapper.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using AutoMapper_Dto.Dto;
+using AutoMapper_Dto.Model;
+using AutoMapper_Dto.Repository.Interface;
+using AutoMapper_Dto.Repository.Repo;
+
+namespace AutoMapper_Dto.AutoMapper
+{
+    //using AutoMapper by extending Profile
+    public class Mapper:Profile
+    {
+        public Mapper()
+        {
+            //for mapping:- 
+
+            // Interface -> Repo
+            CreateMap<IEmployee, EmployeeRepo>();
+            CreateMap<IDepartment,DepartmentRepo>();
+
+            // Model->Dto
+            CreateMap<Employee, EmployeeDto>().ReverseMap();
+            CreateMap<Department, DepartmentDto>().ReverseMap();
+
+
+        }
+
+     
+    }
+}
