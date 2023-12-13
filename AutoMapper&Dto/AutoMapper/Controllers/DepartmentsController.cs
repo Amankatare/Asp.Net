59828ee692a2f5dc5aec732051557afc6407d93a
Aman katare <78726619+Amankatare@users.noreply.github.com>
2023-12-13 15:17:41 +0530

    Add files via upload

---
+++ AutoMapper&Dto/AutoMapper/Controllers/DepartmentsController.cs
@@ -0,0 +1,91 @@
+﻿using AutoMapper;
+using AutoMapper_Dto.Data;
+using AutoMapper_Dto.Dto;
+using AutoMapper_Dto.Model;
+using AutoMapper_Dto.Repository.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AutoMapper_Dto.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentsController : ControllerBase
+    {
+
+        private readonly DatabaseDBContext _dbContext;
+        private readonly IMapper _mapper;
+        public DepartmentsController(DatabaseDBContext dbContext,IMapper mapper)
+        {
+          
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var department = _dbContext.Departments.ToList();
+
+            var departmentsDto = _mapper.Map<List<DepartmentDto>>(department);
+            
+                return Ok(departmentsDto);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetbyId(int id)
+        {
+            var department = _dbContext.Departments.Find(id);
+
+            var departmentsDto = _mapper.Map<DepartmentDto>(department);
+
+            return Ok(departmentsDto);
+        }
+        
+        [HttpPost]
+        public IActionResult Post(Department dept)
+        {
+            var department = _dbContext.Departments.Add(dept);
+            _dbContext.SaveChanges();
+            return Ok(department);
+        }
+
+        
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id,Department dept)
+        {
+            var existingDepartment = _dbContext.Departments.Find(id);
+            if(existingDepartment == null)
+            {
+                return NotFound();  
+            }
+
+            //Method 1 :-
+            //By assigning all the properties one by one
+            existingDepartment .Name = dept.Name;
+            existingDepartment.Location = dept.Location ;
+
+            // Method 2 :-
+            // Update all properties of the existing entity with values from the input 'dept'
+            // _dbContext.Entry(existingDepartment).CurrentValues.SetValues(dept);
+            
+            _dbContext.SaveChanges();
+
+            return Ok(existingDepartment);
+        }
+        
+        [HttpDelete("{id}")]
+        public IActionResult delete(int id)
+        {
+            var dept = _dbContext.Departments.Find(id);
+            if (dept != null) {
+                var department = _dbContext.Departments.Remove(dept);
+                _dbContext.SaveChanges();
+                return Ok(department);
+            }
+
+            return NotFound();
+
+            }
+        }
+}
---
+++ AutoMapper&Dto/AutoMapper/Controllers/EmployeesController.cs
@@ -0,0 +1,99 @@
+﻿using AutoMapper;
+using AutoMapper_Dto.Data;
+using AutoMapper_Dto.Dto;
+using AutoMapper_Dto.Model;
+using AutoMapper_Dto.Repository.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AutoMapper_Dto.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly DatabaseDBContext _dbContext;
+        private readonly IMapper _mapper;
+        public EmployeesController(DatabaseDBContext dbContext, IMapper mapper)
+        {
+
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var employee = _dbContext.Employees.ToList();
+
+            var employeesDto = _mapper.Map<List<EmployeeDto>>(employee);
+
+            return Ok(employeesDto);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetbyId(int id)
+        {
+            var employee = _dbContext.Employees.Find(id);
+
+            var employeesDto = _mapper.Map<EmployeeDto>(employee);
+
+            return Ok(employeesDto);
+        }
+
+        [HttpPost]
+        public IActionResult Post(Employee emp)
+        {
+            var employee = _dbContext.Employees.Add(emp);
+            _dbContext.SaveChanges();
+            return Ok(employee);
+        }
+
+
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, Employee emp)
+        {
+            var existingEmployee = _dbContext.Employees.Find(id);
+            if (existingEmployee == null)
+            {
+                return NotFound();
+            }
+
+            //Method 1 :-
+            //By assigning all the properties one by one
+            existingEmployee.Name = emp.Name;
+
+            existingEmployee.Phone = emp.Phone;
+
+            existingEmployee.Email = emp.Email;
+
+            existingEmployee.Salary = emp.Salary;
+
+
+            // Method 2 :-
+            // Update all properties of the existing entity with values from the input 'emp'
+            // _dbContext.Entry(existingEmployee).CurrentValues.SetValues(emp);
+
+            _dbContext.SaveChanges();
+
+            return Ok(existingEmployee);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult delete(int id)
+        {
+            var emp = _dbContext.Employees.Find(id);
+            if (emp != null)
+            {
+                var employee = _dbContext.Employees.Remove(emp);
+                _dbContext.SaveChanges();
+                return Ok(employee);
+            }
+
+            return NotFound();
+
+        }
+    }
+}
+
