155781c865fc237efb6ce263d843133790137922
Aman katare <78726619+Amankatare@users.noreply.github.com>
2023-12-26 20:41:17 +0530

    Add files via upload

---
+++ MVC2/AutoMapper/Mapper.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using MVC2.Repository.Interface;
+using MVC2.Repository.Repo;
+
+namespace _M_V_C.AutoMapper
+{
+    public class Mapper:Profile
+    {
+        public Mapper()
+        {
+            CreateMap<IStudent, StudentRepo>().ReverseMap();
+        }
+    }
+}
