670e5db6ba28fec926ac25b596470583c500e2d6
Aman katare <78726619+Amankatare@users.noreply.github.com>
2024-01-23 14:04:22 +0530

    Add files via upload

---
+++ ChatWithMe.Entities/Identity/AppUser.cs
@@ -0,0 +1,15 @@
+﻿using ChatWithMe.Entities.Entities;
+using Microsoft.AspNetCore.Identity;
+
+namespace ChatWithMe.Entities.Identity
+{
+    public class AppUser : IdentityUser
+    {
+        public AppUser()
+        {
+            Messages = new HashSet<Message>();
+        }
+
+        public virtual ICollection<Message> Messages { get; set; }
+    }
+}
