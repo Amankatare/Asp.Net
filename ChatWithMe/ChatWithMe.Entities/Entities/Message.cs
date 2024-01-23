da21aead547db2a6b4478cc2fe7aa01891051a12
Aman katare <78726619+Amankatare@users.noreply.github.com>
2024-01-23 14:03:31 +0530

    Add files via upload

---
+++ ChatWithMe/ChatWithMe.Entities/Entities/Message.cs
@@ -0,0 +1,25 @@
+﻿using ChatWithMe.Entities.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatWithMe.Entities.Entities
+{
+    public class Message
+    {
+        public int Id { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Text { get; set; }
+
+        public DateTime When { get; set; }
+
+        public int UserId { get; set; } 
+
+        public virtual AppUser AppUser { get; set; }
+
+    }
+}
